ace8f7c4b40dc81eac768f19302a511928cc08b1
josalferez <jalfpin474@g.educaand.es>
2026-01-23 09:48:13 +0100

    pizarra21 HashMap

---
+++ unidad9.POO/pizarra21/Coche.java
@@ -0,0 +1,23 @@
+public class Coche extends Vehiculo {
+    
+    public Coche(String matricula, String marca) {
+        super(matricula, marca);
+    }
+
+    @Override
+    public String toString() {
+        return "Coche [matricula=" + matricula + "]";
+    }
+
+    @Override
+    public void arrancar() {
+        System.out.println("El coche arranca");
+    }
+
+    @Override
+    public void parar() {
+        System.out.println("El coche se para");
+    }
+
+    
+}
